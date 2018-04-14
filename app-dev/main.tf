39c2ec96780dbff77cede92b55d5f28a368f9adf
Tom Straub <tstraub@cardinalsolutions.com>
2018-04-14 12:58:51 -0500

    Initial

---
+++ app-dev/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "= 0.11.7"
+}
+
+provider "azurerm" {
+  version = "1.3.0"
+}
+
+variable "name" {}
+
+variable "location" {
+  default = "centralus"
+}
+
+resource "azurerm_resource_group" "module" {
+  name     = "${var.name}-rg"
+  location = "${var.location}"
+}
