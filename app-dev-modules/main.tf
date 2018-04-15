482d49fcf1ac360e2741a54cd3939d54e89e1c4d
Tom Straub <tstraub@cardinalsolutions.com>
2018-04-15 13:35:17 -0500

    Test

--- app-dev-modules/main.tf
+++ app-dev-modules/main.tf
@@ -10,13 +10,71 @@ variable "name" {}
 variable "location" {}
 variable "username" {}
 variable "password" {}
+variable "vnet_address_spacing" {}
+variable "subnet_address_prefixes" {}
+
+module "networking" {
+  source  = "app.terraform.io/cardinalsolutions/networking/azurerm"
+  version = "0.0.1"
+  
+  name                 = "${var.name}"
+  location             = "${var.location}"
+  vnet_address_spacing = "${var.vnet_address_spacing}"
+  subnet_address_prefixes = "${var.subnet_address_prefixes}"
+}
 
 module "webserver" {
-    source  = "app.terraform.io/cardinalsolutions/webserver/azurerm"
-    version = "0.0.1"
+  source  = "app.terraform.io/cardinalsolutions/webserver/azurerm"
+  version = "0.0.1"
 
-    location  = "${var.location}"
-    name      = "${var.name}"
-    username = "${var.username}"
-    password = "${var.password}"
+  name      = "${var.name}"
+  location  = "${var.location}"
+  subnet_id = "${module.networking.subnet_ids[0]}"
+  count     = 2
+  username  = "${var.username}"
+  password  = "${var.password}"
+}
+
+module "appserver" {
+  source  = "app.terraform.io/cardinalsolutions/appserver/azurerm"
+  version = "0.0.1"
+
+  name      = "${var.name}"
+  location  = "${var.location}"
+  subnet_id = "${module.networking.subnet_ids[1]}"
+  count     = 1
+  username  = "${var.username}"
+  password  = "${var.password}"
+}
+
+module "dataserver" {
+  source  = "app.terraform.io/cardinalsolutions/dataserver/azurerm"
+  version = "0.0.1"
+
+  name      = "${var.name}"
+  location  = "${var.location}"
+  subnet_id = "${module.networking.subnet_ids[2]}"
+  count     = 1
+  username  = "${var.username}"
+  password  = "${var.password}"
+}
+
+output "networking_vnet" {
+  value = "${module.networking.vnet_id}"
+}
+
+output "networking_subnets" {
+  value = "${module.networking.subnet_ids}"
+}
+
+output "webserver_vms" {
+  value = "${module.webserver.vms_id}"
+}
+
+output "appserver_vms" {
+  value = "${module.appserver.vms_id}"
+}
+
+output "dataserver_vms" {
+  value = "${module.dataserver.vms_id}"
 }
