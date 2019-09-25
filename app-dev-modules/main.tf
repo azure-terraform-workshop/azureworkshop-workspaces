701eef736ff9e0c2344b636ece424dfc250e5f4d
Tom Straub <tom.straub@insight.com>
2019-09-25 08:48:50 -0500

    Update main.tf

--- app-dev-modules/main.tf
+++ app-dev-modules/main.tf
@@ -2,6 +2,7 @@ variable "name" {}
 variable "location" {}
 variable "username" {}
 variable "password" {}
+variable "vm_size" {}
 
 variable "vnet_address_spacing" {
   type = "list"
@@ -12,79 +13,50 @@ variable "subnet_address_prefixes" {
 }
 
 module "networking" {
-  source  = "app.terraform.io/cardinalsolutions/networking/azurerm"
-  version = "0.0.1"
+  source  = "cmm-training.digitalinnovation.dev/YOUR_ORG_NAME/networking/azurerm"
+  version = "0.12.0"
 
-  name                    = "${var.name}"
-  location                = "${var.location}"
-  vnet_address_spacing    = "${var.vnet_address_spacing}"
-  subnet_address_prefixes = "${var.subnet_address_prefixes}"
+  name                    = var.name
+  location                = var.location
+  vnet_address_spacing    = var.vnet_address_spacing
+  subnet_address_prefixes = var.subnet_address_prefixes
 }
-
+  
 module "webserver" {
-  source  = "app.terraform.io/cardinalsolutions/webserver/azurerm"
-  version = "0.0.1"
+  source  = "cmm-training.digitalinnovation.dev/YOUR_ORG_NAME/webserver/azurerm"
+  version = "0.12.0"
 
-  name      = "${var.name}"
-  location  = "${var.location}"
-  subnet_id = "${module.networking.subnet-ids[0]}"
-  count     = 2
-  username  = "${var.username}"
-  password  = "${var.password}"
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[0]
+  vm_count  = 1
+  size      = var.vm_size
+  username  = var.username
+  password  = var.password
 }
 
 module "appserver" {
-  source  = "app.terraform.io/cardinalsolutions/appserver/azurerm"
-  version = "0.0.1"
+  source  = "cmm-training.digitalinnovation.dev/YOUR_ORG_NAME/appserver/azurerm"
+  version = "0.12.0"
 
-  name      = "${var.name}"
-  location  = "${var.location}"
-  subnet_id = "${module.networking.subnet-ids[1]}"
-  count     = 1
-  username  = "${var.username}"
-  password  = "${var.password}"
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[1]
+  vm_count  = 1
+  size      = var.vm_size
+  username  = var.username
+  password  = var.password
 }
 
 module "dataserver" {
-  source  = "app.terraform.io/cardinalsolutions/dataserver/azurerm"
-  version = "0.0.1"
+  source  = "cmm-training.digitalinnovation.dev/YOUR_ORG_NAME/dataserver/azurerm"
+  version = "0.12.0"
 
-  name      = "${var.name}"
-  location  = "${var.location}"
-  subnet_id = "${module.networking.subnet-ids[2]}"
-  count     = 1
-  username  = "${var.username}"
-  password  = "${var.password}"
-}
-
-output "networking_vnet" {
-  value = "${module.networking.virtualnetwork-ids}"
-}
-
-output "networking_subnets" {
-  value = "${module.networking.subnet-ids}"
-}
-
-output "webserver-vm-ids" {
-  value = "${module.webserver.vm-ids}"
-}
-
-output "webserver-private-ips" {
-  value = "${module.webserver.private-ips}"
-}
-
-output "appserver-vm-ids" {
-  value = "${module.appserver.vm-ids}"
-}
-
-output "appserver-private-ips" {
-  value = "${module.appserver.private-ips}"
-}
-
-output "dataserver-vm-ids" {
-  value = "${module.dataserver.vm-ids}"
-}
-
-output "dataserver-private-ips" {
-  value = "${module.dataserver.private-ips}"
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[2]
+  vm_count  = 1
+  size      = var.vm_size
+  username  = var.username
+  password  = var.password
 }
